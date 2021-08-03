c2f716921f552d9953688d01dfe2cc36625d163d
FireFlashing <filippoettoreg@gmail.com>
2021-08-03 23:53:56 +0200

    Added function_add(), methods to calculate touch and offset_pos property

--- main_script.py
+++ main_script.py
@@ -16,21 +16,26 @@ class DynamicObject():
     custom_pos=None
     custom_size=None
     updated=False
-    #FIXME: what if instead of vertical offset, I use vertical_size and translate it through translate_function?
-    def __init__(self,this_object,current_canvas,nickname=None,proportions=None,custom_size=None,custom_pos=None):
+    offset_pos=None
+    def __init__(self,this_object,current_canvas,nickname=None,proportions=None,custom_size=None,custom_pos=None,offset_pos=None,on_touch=None,object_dict=None):
         """:param this_object: a kivy.graphics object
         :param current_canvas: the widget in which the object is
         :param str nickname: a name to recognize this object; if not set, is equal to the object's uid
-        :param tuple custom_size: a tuple of two functions which define the object's size when the update_space() method is called. Both can be set as None.
-        :param tuple custom_pos: a tuple of two functions which define the object's pos when the update_space() method is called. Both can be set as None.
+        :param tuple custom_size: a tuple of two functions which define the object's size when the update_space() method is called. Both can be set as None. For more info about functions, see translate_function()
+        :param tuple custom_pos: a tuple of two functions which define the object's pos when the update_space() method is called. Both can be set as None. For more info about functions, see translate_function()
         :param proportions: proportions by which the object is downscaled in the format "n:n"
+        :param on_touch: a dictionary which says which functions to call if the object is touched in a certain way. Here's an example: {"left":{"call":"a_function()"}}. In order to mention the current object, use "dynamicobject" or "<self>"
+        :param object_dict: a dictionary which stores various data about the non-graphical object
         """
         self.uid=this_object.uid
         self.object_class=this_object
         self.nickname=nickname if nickname!=None else this_object.uid
         self.proportions=proportions if proportions!=None else None
         self.custom_pos=custom_pos if custom_pos!=None else None
+        self.offset_pos=offset_pos if offset_pos!=None else (None,None)
         self.custom_size=custom_size if custom_size!=None else None
+        self.on_touch=on_touch if on_touch!=None else None
+        self.object_dict=object_dict if object_dict!=None else None
         current_canvas.objects_list.append(self)
     
     def update_space(self,current_canvas,current_window):
@@ -40,7 +45,6 @@ class DynamicObject():
             self.update_pos(current_canvas,current_window)
             d_print("Space updated for object %s; size(%i,%i); pos(%i,%i)"%(self.nickname,self.object_class.size[0],self.object_class.size[1],self.object_class.pos[0],self.object_class.pos[1]))
         self.updated=True
-
     def update_size(self,current_canvas,current_window):
         """Updates object size depending on its properties"""
         size_x=self.object_class.size[0]
@@ -69,8 +73,42 @@ class DynamicObject():
             if self.custom_pos[1]!=None:
                 self.custom_pos=(self.custom_pos[0],translate_function(self.custom_pos[1],axis=1))
                 pos_y=eval(self.custom_pos[1])
+        if self.offset_pos!=None:
+            if self.offset_pos[0]!=None:
+                pos_x=pos_x+eval(translate_function(self.offset_pos[0],axis=0))
+            if self.offset_pos[1]!=None:
+                pos_y=pos_y+eval(translate_function(self.offset_pos[1],axis=1))
 
         self.object_class.pos=(pos_x,pos_y)
+
+    def was_touched(self,touch):
+        """Returns a bool which tells if this object has been touched"""
+        if self.on_touch!=None:
+            if touch.button in self.on_touch:
+                if str(type(self.object_class))=="<class 'kivy.graphics.vertex_instructions.Ellipse'>":
+                    #FIXME: not working with angled shapes
+                    this_center=(self.object_class.pos[0]+(self.object_class.size[0]/2),self.object_class.pos[1]+(self.object_class.size[1]/2))
+                    if ((touch.pos[0]-this_center[0])**2)/((self.object_class.size[0]/2)**2)+((touch.pos[1]-this_center[1])**2)/((self.object_class.size[1]/2)**2) <= 1: #Reference matematica: https://math.stackexchange.com/questions/76457/check-if-a-point-is-within-an-ellipse
+                        return True
+                    else:
+                        return False
+                elif str(type(self.object_class))=="<class 'kivy.graphics.vertex_instructions.Rectangle'>":
+                    #FIXME: not working with angled shapes
+                    if (self.object_class.pos[0]<=touch.pos[0] and self.object_class.pos[1]<=touch.pos[1]) and (self.object_class.size[0]+self.object_class.pos[0]>=touch.pos[0] and self.object_class.size[1]+self.object_class.pos[1]>=touch.pos[1]):
+                        return True
+                    else:
+                        return False
+            else:
+                return False
+        else:
+            return False
+    def touch_function(self,touch):
+        """Outputs an eventual touch function for the touched object"""
+        output_value="d_print('Empty touch function')"
+        if self.on_touch!=None:
+            if touch.button in self.on_touch:
+                output_value="self."+self.on_touch[touch.button]["call"].replace("<self>","dynamicobject")
+        return output_value
 
     def get_object_ind_by_name(self,name,current_canvas):
         """Get an object's index in a canvas' objects_list searching by its name. If no match is found, the returned value is -1"""
@@ -81,13 +119,13 @@ class DynamicObject():
                 output_value=i
             i+=1
         return output_value
-    def get_object_class(self,current_canvas,nickname=None,space_update=False):
+    def get_object_class(self,current_canvas,current_window,nickname=None,space_update=False):
         """Returns an object class, found by using the given parameters"""
         output_value=None
         if nickname!=None:
             output_value=current_canvas.objects_list[self.get_object_ind_by_name(nickname,current_canvas)]
         if space_update:
-            output_value.update_space(current_canvas)
+            output_value.update_space(current_canvas,current_window)
         return output_value
 
 def convert_numeric_string(numeric_string:str,total_amount=0):
@@ -131,17 +169,30 @@ def translate_function(this_function,axis=0):
     $$n/n$$: requires axis index and outputs the given fraction of the given axis
     $$extreme(class_object)$$: requires axis index and outputs the right/top border of the object
     
-    <<object_nickname>>: refers to the object_class of the dynamic object with given nickname. You can also use <<self>> to refer to the current object"""
+    <<object_nickname>>: refers to the object_class of the dynamic object with given nickname. You can also use <<self>> to refer to the current object
+    Window: automatically replaced with current_window"""
     output_value=this_function
     last_function=""
     while output_value!=last_function:
+        d_print(output_value)
         last_function=output_value
+        output_value=output_value.replace("§§","")
         output_value=output_value.replace("<<self>>","self.object_class")
-        output_value=sub("<<(.*)>>","self.get_object_class(current_canvas,nickname=\"\g<1>\",space_update=True).object_class",output_value)
+        output_value=sub("<<(.*)>>","self.get_object_class(current_canvas,current_window,nickname=\"\g<1>\",space_update=True).object_class",output_value)
         output_value=sub("\$\$extreme\((.*)\)\$\$","(\g<1>.size[%i]+\g<1>.pos[%i])"%(axis,axis),output_value)
         output_value=output_value.replace("$$center$$","(current_window.size[%i]/2)-(self.object_class.size[%i]/2)"%(axis,axis)) if "$$center$$"in output_value else output_value
-        output_value=sub("\$\$(\d*)/(\d*)\$\$","(int(\g<1>)*current_window.size[%i])/int(\g<2>)"%(axis),output_value)
+        output_value=sub("\$\$(\-?\d*)/(\d*)\$\$","(int(\g<1>)*current_window.size[%i])/int(\g<2>)"%(axis),output_value)
+        output_value=output_value.replace("Window","current_window")
     if ("$$" in last_function or "<<" in last_function) or ("$$" in last_function and "<<" in last_function):
         w_print("Unparsable function string: '%s'"%(last_function))
     #d_print(output_value)
+    return output_value
+def function_add(base_function,value):
+    """Sums given value to the first value between "§§" in the base_function
+    
+    Example: function_add("§§10§§/100",10) outputs "§§20§§/100" """
+    output_value=base_function
+    base_search=search("§§(\-?\d*)§§",str(base_function))
+    if base_search!=None:
+        output_value=sub("§§(\-?\d*)§§","§§%i§§"%(int(base_search.group(1))+value),output_value)
     return output_value
